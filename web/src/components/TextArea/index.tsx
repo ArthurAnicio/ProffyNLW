34e965efd4d3e385879bc26e699cb431dab1e20f
Artzuca <arthuranicio06@gmsil.com>
2023-12-21 16:13:05 -0300

    Fixed Error to show bio in Teacher Form

--- web/src/components/TextArea/index.tsx
+++ web/src/components/TextArea/index.tsx
@@ -1,18 +1,19 @@
-import React, { TextareaHTMLAttributes } from "react";
+import React, { TextareaHTMLAttributes } from 'react'
+
 import './styles.css'
 
-interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement>{
-    label : string;
-    name?: string;
-    
-}
-const TextArea: React.FC<TextAreaProps> = (props, ...rest) => {
-    return(
-    <div className='Textarea-block'>
-        <label htmlFor={props.name}>{props.label}</label>
-        <textarea  id={props.name} {...rest} />
-    </div>
-    )
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+    id: string;
+    label: string;
 }
 
-export default TextArea
+const TextArea: React.FC<TextAreaProps> = ({ id, label, ...rest }) => {
+    return (
+      <div className="textarea-block">
+          <label htmlFor={id}>{label}</label>
+          <textarea id={id} {...rest} />
+      </div>
+    );
+}
+
+export default TextArea;
